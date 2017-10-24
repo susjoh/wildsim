02d8d4bb695f152efab6d78956a336908b9d20d8
Susan Johnston <Susan.Johnston@ed.ac.uk>
2017-10-24 01:33:35 +0100

    Updated to deal with larger files

---
+++ R/RunSED.R
@@ -0,0 +1,10 @@
+#' RunSED: Run sed in Windows
+#' @param input.string Command to be passed to sed.
+#' @export
+#'
+
+RunSED <- function(input.string){
+
+
+
+}
--- R/parseQMSIM.R
+++ R/parseQMSIM.R
@@ -1,9 +1,15 @@
-
-model.name <- "Scenario_0_10k_1000"
+#' parseQMSIM: Parse files in a QMSIM directory.
+#' @param model.name string. This is the name of the directory containing the simulation information.
+#' @param markerfile string. Default is "p1_mrk_001.txt".
+#' @param phenofile string. Default is "p1_data_001.txt"
+#' @param flanking.window integer
+#' @export
+#'
 
 parseQMSIM <- function(model.name,
                        markerfile = "p1_mrk_001.txt",
-                       phenofile = "p1_data_001.txt"){
+                       phenofile = "p1_data_001.txt",
+                       flanking.window = 10){
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
   # 0. Load libraries, functions and data information   #
@@ -13,12 +19,17 @@ parseQMSIM <- function(model.name,
   require(plyr)
   require(ggplot2)
 
+  current.wd <- getwd()
+
   setwd(model.name)
+
+  model.name <- gsub("\\\\", "/", model.name)
+  model.name <- strsplit(model.name, split = "/")[[1]]
+  model.name <- model.name[length(model.name)]
 
   marker.prefix <- gsub(".txt", "", markerfile)
   qtl.prefix <- gsub("mrk", "qtl", marker.prefix)
 
-  flanking.window <- 10
 
   report <- readLines("report.txt")
   params <- readLines(paste0(model.name, ".prm"))
@@ -175,10 +186,10 @@ parseQMSIM <- function(model.name,
 
   if(Sys.info()["sysname"] == "Windows") {
 
-    system("del *.ped")
-    system("del *.map")
-    system("del *.sexchange.txt")
-    system("del *.nosex")
+    system("cmd", input = "del *.ped")
+    system("cmd", input = "del *.map")
+    system("cmd", input = "del *.sexchange.txt")
+    system("cmd", input = "del *.nosex")
 
   } else {
 
@@ -192,7 +203,7 @@ parseQMSIM <- function(model.name,
 
   #~~ Create a directory for results
 
-  system("mkdir results")
+  system(paste0("mkdir ../../", model.name, ".results"))
 
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -216,7 +227,7 @@ parseQMSIM <- function(model.name,
   data1.dist <- as.dist(0.5 - data1.gkin)
   data1.mds <- cmdscale(data1.dist)
 
-  png("results/Population_Structure.png", width = 4, height = 4, units = "in", res = 300)
+  png(paste0("../../", model.name, ".results/Population_Structure.png"), width = 4, height = 4, units = "in", res = 300)
   plot(data1.mds)
   dev.off()
 
@@ -234,8 +245,8 @@ parseQMSIM <- function(model.name,
   model1.results <- results(model1)
   model1.results$SNP.Name <- row.names(model1.results)
 
-  png("results/GWAS_Markers_Only.png", width = 8, height = 4, units = "in", res = 300)
-  FullSummary(model1)
+  png(paste0("../../", model.name, ".results/GWAS_Markers_Only.png"), width = 8, height = 4, units = "in", res = 300)
+  FullGwasSummary(model1)
   dev.off()
 
   #~~ Model with merged data
@@ -245,8 +256,8 @@ parseQMSIM <- function(model.name,
   model2.results <- results(model2)
   model2.results$SNP.Name <- row.names(model2.results)
 
-  png("results/GWAS_Markers_with_QTL.png", width = 8, height = 4, units = "in", res = 300)
-  FullSummary(model2, qtl.ids = qtl.positions$ID)
+  png(paste0("../../", model.name, ".results/GWAS_Markers_with_QTL.png"), width = 8, height = 4, units = "in", res = 300)
+  FullGwasSummary(model2, qtl.ids = qtl.positions$ID)
   dev.off()
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -293,12 +304,27 @@ parseQMSIM <- function(model.name,
 
   for(i in 1:nrow(qtl.positions)){
 
-    focal.snp <- which(model1.results$Chromosome == qtl.positions$Chr[i] &
-                         model1.results$Position > qtl.positions$BP[i])[1]
+    x <- subset(model1.results, Chromosome == qtl.positions$Chr[1])
 
-    qtl.positions$QTL.Position[i] <- model1.results$SNP.Name[focal.snp]
+    focal.snp <- which(x$Position > qtl.positions$BP[i])[1]
 
-    snp.list <- model1.results$SNP.Name[(focal.snp - flanking.window + 1):(focal.snp + flanking.window)]
+    qtl.positions$QTL.Position[i] <- x$SNP.Name[focal.snp]
+
+    start.pos <- (focal.snp - flanking.window + 1)
+    stop.pos  <- (focal.snp + flanking.window)
+
+
+    if(start.pos < 1){
+      start.pos <- 1
+      stop.pos <- flanking.window*2
+    }
+
+    if(stop.pos > nrow(x)){
+      start.pos <- nrow(x) - (flanking.window*2)
+      stop.pos <- nrow(x)
+    }
+
+    snp.list <- x$SNP.Name[start.pos:stop.pos]
     write.table(snp.list, paste0(marker.prefix, ".snplist.", i, ".txt"), row.names = F, col.names = F, quote = F)
 
     RunGCTA(paste0("--bfile ", marker.prefix, " --make-grm-gz --extract ", marker.prefix, ".snplist.", i, ".txt --out ", marker.prefix, ".snplist.", i , "_GRM"))
@@ -308,21 +334,24 @@ parseQMSIM <- function(model.name,
                       marker.prefix, ".snplist.", i , "_wo_GRM"),
                paste0(marker.prefix, ".snplist.", i , "_GRMs.txt"))
 
-    RunGCTA(paste0("--mgrm-gz ", marker.prefix, ".snplist.", i , "_GRMs.txt --pheno ", marker.prefix, ".phenochange.txt --reml --out ", marker.prefix, "_GRMs_res"))
+    try(RunGCTA(paste0("--mgrm-gz ", marker.prefix, ".snplist.", i , "_GRMs.txt --pheno ", marker.prefix, ".phenochange.txt --reml --out ", marker.prefix, "_GRMs_res")))
 
-    gcta.res.temp <- read.table(paste0(marker.prefix, "_GRMs_res.hsq"), header = T, sep = "\t", fill = T)
+    if(file.exists(paste0(marker.prefix, "_GRMs_res.hsq"))){
+      gcta.res.temp <- read.table(paste0(marker.prefix, "_GRMs_res.hsq"), header = T, sep = "\t", fill = T)
 
-    qtl.positions$Vqtl[i]    <- gcta.res.temp[1,2]
-    qtl.positions$Vqtl.SE[i] <- gcta.res.temp[1,3]
-    qtl.positions$Va[i]      <- gcta.res.temp[2,2]
-    qtl.positions$Va.SE[i]   <- gcta.res.temp[2,3]
-    qtl.positions$Vp[i]      <- gcta.res.temp[4,2]
-    qtl.positions$Vp.SE[i]   <- gcta.res.temp[4,3]
-    qtl.positions$qtl2[i]    <- gcta.res.temp[5,2]
-    qtl.positions$qtl2.SE[i] <- gcta.res.temp[5,3]
-    qtl.positions$h2[i]      <- gcta.res.temp[6,2]
-    qtl.positions$h2.SE[i]   <- gcta.res.temp[6,3]
-    qtl.positions$LogL[i]    <- gcta.res.temp[8,2]
+      qtl.positions$Vqtl[i]    <- gcta.res.temp[1,2]
+      qtl.positions$Vqtl.SE[i] <- gcta.res.temp[1,3]
+      qtl.positions$Va[i]      <- gcta.res.temp[2,2]
+      qtl.positions$Va.SE[i]   <- gcta.res.temp[2,3]
+      qtl.positions$Vp[i]      <- gcta.res.temp[4,2]
+      qtl.positions$Vp.SE[i]   <- gcta.res.temp[4,3]
+      qtl.positions$qtl2[i]    <- gcta.res.temp[5,2]
+      qtl.positions$qtl2.SE[i] <- gcta.res.temp[5,3]
+      qtl.positions$h2[i]      <- gcta.res.temp[6,2]
+      qtl.positions$h2.SE[i]   <- gcta.res.temp[6,3]
+      qtl.positions$LogL[i]    <- gcta.res.temp[8,2]
+
+    }
 
     system(paste0("rm ", marker.prefix, ".snplist.", i , "*"))
 
@@ -347,9 +376,28 @@ parseQMSIM <- function(model.name,
 
   for(i in 1:nrow(qtl.positions.merged)){
 
-    focal.snp <- which(model2.results$SNP.Name == qtl.positions.merged$ID[i])
+    x <- subset(model2.results, Chromosome == qtl.positions$Chr[i])
 
-    qtl.positions.merged$QTL.Position[i] <- model2.results$SNP.Name[focal.snp]
+    focal.snp <- which(x$SNP.Name == qtl.positions.merged$ID[i])
+
+    qtl.positions.merged$QTL.Position[i] <- x$SNP.Name[focal.snp]
+
+    start.pos <- (focal.snp - flanking.window)
+    stop.pos  <- (focal.snp + flanking.window)
+
+
+    if(start.pos < 1){
+      start.pos <- 1
+      stop.pos <- flanking.window*2
+    }
+
+    if(stop.pos > nrow(x)){
+      start.pos <- nrow(x) - (flanking.window*2)
+      stop.pos <- nrow(x)
+    }
+
+    snp.list <- x$SNP.Name[start.pos:stop.pos]
+
 
     snp.list <- model1.results$SNP.Name[(focal.snp + 1 - flanking.window):(focal.snp + flanking.window)]
     write.table(snp.list, paste0(marker.prefix, ".snplist.", i, ".txt"), row.names = F, col.names = F, quote = F)
@@ -361,21 +409,23 @@ parseQMSIM <- function(model.name,
                       marker.prefix, ".snplist.", i , "_merged_wo_GRM"),
                paste0(marker.prefix, ".snplist.", i , "_merged_GRMs.txt"))
 
-    RunGCTA(paste0("--mgrm-gz ", marker.prefix, ".snplist.", i , "_merged_GRMs.txt --pheno ", marker.prefix, ".phenochange.txt --reml --out ", marker.prefix, "_merged_GRMs_res"))
+    try(RunGCTA(paste0("--mgrm-gz ", marker.prefix, ".snplist.", i , "_merged_GRMs.txt --pheno ", marker.prefix, ".phenochange.txt --reml --out ", marker.prefix, "_merged_GRMs_res")))
 
-    gcta.res.temp <- read.table(paste0(marker.prefix, "_merged_GRMs_res.hsq"), header = T, sep = "\t", fill = T)
+    if(file.exists(paste0(marker.prefix, "_merged_GRMs_res.hsq"))){
+      gcta.res.temp <- read.table(paste0(marker.prefix, "_merged_GRMs_res.hsq"), header = T, sep = "\t", fill = T)
 
-    qtl.positions.merged$Vqtl[i]    <- gcta.res.temp[1,2]
-    qtl.positions.merged$Vqtl.SE[i] <- gcta.res.temp[1,3]
-    qtl.positions.merged$Va[i]      <- gcta.res.temp[2,2]
-    qtl.positions.merged$Va.SE[i]   <- gcta.res.temp[2,3]
-    qtl.positions.merged$Vp[i]      <- gcta.res.temp[4,2]
-    qtl.positions.merged$Vp.SE[i]   <- gcta.res.temp[4,3]
-    qtl.positions.merged$qtl2[i]    <- gcta.res.temp[5,2]
-    qtl.positions.merged$qtl2.SE[i] <- gcta.res.temp[5,3]
-    qtl.positions.merged$h2[i]      <- gcta.res.temp[6,2]
-    qtl.positions.merged$h2.SE[i]   <- gcta.res.temp[6,3]
-    qtl.positions.merged$LogL[i]    <- gcta.res.temp[8,2]
+      qtl.positions.merged$Vqtl[i]    <- gcta.res.temp[1,2]
+      qtl.positions.merged$Vqtl.SE[i] <- gcta.res.temp[1,3]
+      qtl.positions.merged$Va[i]      <- gcta.res.temp[2,2]
+      qtl.positions.merged$Va.SE[i]   <- gcta.res.temp[2,3]
+      qtl.positions.merged$Vp[i]      <- gcta.res.temp[4,2]
+      qtl.positions.merged$Vp.SE[i]   <- gcta.res.temp[4,3]
+      qtl.positions.merged$qtl2[i]    <- gcta.res.temp[5,2]
+      qtl.positions.merged$qtl2.SE[i] <- gcta.res.temp[5,3]
+      qtl.positions.merged$h2[i]      <- gcta.res.temp[6,2]
+      qtl.positions.merged$h2.SE[i]   <- gcta.res.temp[6,3]
+      qtl.positions.merged$LogL[i]    <- gcta.res.temp[8,2]
+    }
 
     system(paste0("rm ", marker.prefix, ".snplist.", i , "*"))
 
@@ -405,8 +455,8 @@ parseQMSIM <- function(model.name,
     coord_cartesian(ylim = c(0, max.var), xlim = c(0, max.var))+
     ggtitle("QTL Effects on with QTL Included")
 
-  png("results/QTL_Effect_Sizes.png", width = 8, height = 4, units = "in", res = 300)
-  multiplot(p1, p2, cols = 2)
+  png(paste0("../../", model.name, ".results/QTL_Effect_Sizes.png"), width = 8, height = 4, units = "in", res = 300)
+  try(multiplot(p1, p2, cols = 2))
   dev.off()
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -418,7 +468,7 @@ parseQMSIM <- function(model.name,
 
   save(marker.only.model, marker.qtl.model, data1.gkin, gcta.res, gcta.res.merged, abeldata, abeldata,
        phenotab, qtl.positions, qtl.positions.merged, flanking.window, params, report,
-       file = paste0("results/", model.name, ".RData"))
+       file = paste0("../../", model.name, ".results/", model.name, ".RData"))
 
   #~~ Remove files that were generated above.
 
@@ -426,14 +476,14 @@ parseQMSIM <- function(model.name,
   remove.vec <- remove.vec[grep(".bed|.fam|.bim|.phenochange|.hsq|.gen|.gz|.id|.log", remove.vec)]
 
   if(Sys.info()["sysname"] == "Windows") {
-    system(paste0("del ", paste0(remove.vec, collapse = " ")))
+    system("cmd", input = paste0("del ", paste0(remove.vec, collapse = " ")))
   } else {
     system(paste0("rm ", paste0(remove.vec, collapse = " ")))
   }
 
+  setwd(current.wd)
 
 }
-
 
 
 
