867d5ffd612891896fddecec3b00c6192382a67a
Susan Johnston <Susan.Johnston@ed.ac.uk>
2017-11-11 21:47:12 +0000

    added capability to specify SNPs and IDs

--- R/estimateGenomicHeritability.R
+++ R/estimateGenomicHeritability.R
@@ -1,45 +1,54 @@
 #' estimateGenomicHeritability: create a kinship matrix using ibs in R
 #' @param markerfile Path to markerfile
+#' @param snpvec Vector of SNPs to include (all others excluded). Optional
+#' @param idvec Vector of IDs to include (all others excluded). Optional
 #' @param merged Default FALSE. If merged, will include the QTL markers.
 #' @import reshape
 #' @import plyr
 #' @export
 #'
 
-estimateGenomicHeritability <- function(markerfile, merged = F){
+estimateGenomicHeritability <- function(markerfile, snpvec = NULL, idvec = NULL, outfile = NULL, merged = F){
 
   require(reshape)
   require(plyr)
 
   marker.prefix <- gsub(".txt", "", markerfile)
 
+  if(is.null(outfile)) outfile <- marker.prefix
+  if(!is.null(snpvec)) writeLines(snpvec, paste0(marker.prefix, ".snpvec"))
+  if(!is.null(idvec )) writeLines(idvec, paste0(marker.prefix, ".idvec"))
+
+  extra.piece <- paste(c(ifelse(!is.null(snpvec), paste0(" --extract ", marker.prefix, ".snpvec"), ""),
+                         ifelse(!is.null(idvec), paste0(" --keep ", marker.prefix, ".idvec"), "")), collapse = " ")
+
   if(!merged){
 
-    if(!file.exists(paste0(marker.prefix, "_GRM.grm.gz"))){
+    if(!file.exists(paste0(outfile, "_GRM.grm.gz"))){
 
-      RunGCTA(paste0("--bfile ", marker.prefix, " --make-grm-gz --out ", marker.prefix, "_GRM"))
+      RunGCTA(paste0("--bfile ", marker.prefix, extra.piece, " --make-grm-gz --out ", outfile, "_GRM"))
 
     }
 
-    RunGCTA(paste0("--grm-gz ", marker.prefix, "_GRM --pheno ",
+    RunGCTA(paste0("--grm-gz ", outfile, "_GRM --pheno ",
                    marker.prefix, ".phenochange.txt --reml --out ",
-                   marker.prefix, "_GRM_res"))
+                   outfile, "_GRM_res"))
 
-    gcta.res <- read.table(paste0(marker.prefix, "_GRM_res.hsq"), header = T, sep = "\t", fill = T)
+    gcta.res <- read.table(paste0(outfile, "_GRM_res.hsq"), header = T, sep = "\t", fill = T)
     gcta.res
   } else {
 
-    if(!file.exists(paste0(marker.prefix, "_merged_GRM.grm.gz"))){
+    if(!file.exists(paste0(outfile, "_merged_GRM.grm.gz"))){
 
-      RunGCTA(paste0("--bfile ", marker.prefix, "_merged --make-grm-gz --out ", marker.prefix, "_merged_GRM"))
+      RunGCTA(paste0("--bfile ", marker.prefix, "_merged ", extra.piece, " --make-grm-gz --out ", outfile, "_merged_GRM"))
 
     }
 
-    RunGCTA(paste0("--grm-gz ", marker.prefix, "_merged_GRM --pheno ",
+    RunGCTA(paste0("--grm-gz ", outfile, "_merged_GRM --pheno ",
                    marker.prefix, ".phenochange.txt --reml --out ",
-                   marker.prefix, "_merged_GRM_res"))
+                   outfile, "_merged_GRM_res"))
 
-    gcta.res <- read.table(paste0(marker.prefix, "_merged_GRM_res.hsq"), header = T, sep = "\t", fill = T)
+    gcta.res <- read.table(paste0(outfile, "_merged_GRM_res.hsq"), header = T, sep = "\t", fill = T)
     gcta.res
 
   }
