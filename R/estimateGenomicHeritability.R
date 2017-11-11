d5361066f322dcff3c174a3d7a889ac4accd9888
Susan Johnston <Susan.Johnston@ed.ac.uk>
2017-11-11 12:39:33 +0000

    fixed file.exists bug

--- R/estimateGenomicHeritability.R
+++ R/estimateGenomicHeritability.R
@@ -10,14 +10,13 @@ estimateGenomicHeritability <- function(markerfile, merged = F){
 
   marker.prefix <- gsub(".txt", "", markerfile)
 
-  if(!file.exists(paste0(marker.prefix, "_GRM.grm.gz"))){
-
-    RunGCTA(paste0("--bfile ", marker.prefix, " --make-grm-gz --out ", marker.prefix, "_GRM"))
-    RunGCTA(paste0("--bfile ", marker.prefix, "_merged --make-grm-gz --out ", marker.prefix, "_merged_GRM"))
-
-  }
-
   if(!merged){
+
+    if(!file.exists(paste0(marker.prefix, "_GRM.grm.gz"))){
+
+      RunGCTA(paste0("--bfile ", marker.prefix, " --make-grm-gz --out ", marker.prefix, "_GRM"))
+
+    }
 
     RunGCTA(paste0("--grm-gz ", marker.prefix, "_GRM --pheno ",
                    marker.prefix, ".phenochange.txt --reml --out ",
@@ -26,6 +25,12 @@ estimateGenomicHeritability <- function(markerfile, merged = F){
     gcta.res <- read.table(paste0(marker.prefix, "_GRM_res.hsq"), header = T, sep = "\t", fill = T)
     gcta.res
   } else {
+
+    if(!file.exists(paste0(marker.prefix, "_merged_GRM.grm.gz"))){
+
+      RunGCTA(paste0("--bfile ", marker.prefix, "_merged --make-grm-gz --out ", marker.prefix, "_merged_GRM"))
+
+    }
 
     RunGCTA(paste0("--grm-gz ", marker.prefix, "_merged_GRM --pheno ",
                    marker.prefix, ".phenochange.txt --reml --out ",
